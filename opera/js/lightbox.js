b3124a390cf65c00e4bcb70ea97217a5e31a9b06
gyunxiu <gyunxiu@gmail.com>
2018-03-08 10:55:55 -0800

    js

--- opera/js/lightbox.js
+++ opera/js/lightbox.js
@@ -19,7 +19,7 @@
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory(require('jquery'));
+        module.exports = factory(require('jquery')); 
     } else {
         // Browser globals (root is window)
         root.lightbox = factory(root.jQuery);
